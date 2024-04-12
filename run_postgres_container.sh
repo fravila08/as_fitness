6f0d82214cba1109368a25db8d3aaa7612187b25
fravila08 <fr4v1l4@gmail.com>
2024-04-12 16:19:03 -0700

    create postgresql container with setup script

--- run_postgres_container.sh
+++ run_postgres_container.sh
@@ -7,7 +7,7 @@ source .env
 docker build -t postgres_img -f docker/Dockerfile_postgresql .
 
 # Run the PostgreSQL container
-docker run -e POSTGRES_PASSWORD=$DB_PASSWORD -e POSTGRES_DB=$DB_NAME -e POSTGRES_USER=$DB_USER --rm --name postgres -d -p 5433:5432 postgres_img
+docker run -e POSTGRES_PASSWORD=$DB_PASSWORD -e POSTGRES_DB=$DB_NAME -e POSTGRES_USER=$DB_USER --rm --name postgres -d -p $DB_PORT:5432 postgres_img
 
 # Connect to the PostgreSQL database using psql
 psql -h localhost -p 5433 -d $DB_NAME -U $DB_USER -W

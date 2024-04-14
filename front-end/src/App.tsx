ec2efd1104272b2c3fc62a2936e6dd870fe87a22
fravila08 <fr4v1l4@gmail.com>
2024-04-14 11:50:52 -0700

    docker-compose and Dockerfiles with scripts to run full stack app ready to go

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import axios from "axios";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  console.log("hello world");
+
+  useEffect(()=>{
+    axios.get("http://localhost/api/")
+      .then((response)=>{
+        console.log(response.data)
+      })
+      .catch((err)=>{
+        alert(err.message)
+      })
+      .finally(()=>{
+        console.log("success")
+      })
+  }, [])
 
   return (
     <>

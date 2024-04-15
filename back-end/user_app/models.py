9b6aa2d6e5af34b89e012df5934e570374d19322
fravila08 <fr4v1l4@gmail.com>
2024-04-15 09:50:35 -0700

    Utilize UUID field for user id

--- back-end/user_app/models.py
+++ back-end/user_app/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 
 # Create your models here.
 class App_user(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     password = models.CharField(max_length=8)
     email = models.EmailField(blank=False, unique=True)
     display_name = models.CharField(max_length=25)
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS=[]
+    REQUIRED_FIELDS=["display_name"]
+
+    def __str__(self):
+        return self.display_name
